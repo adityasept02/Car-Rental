d3f8f31f3437aa59b6fc3fae8ac5579e6ffa8204
adityasept02 <adityasept02@gmail.com>
2024-11-16 23:18:29 +0530

    first commit

---
+++ src/config.js
@@ -0,0 +1,21 @@
+export const API = "http://localhost:8000/api";
+// export const API = "https://buy-sell-one.vercel.app/api";
+export const FIREBASE_API_KEY = "AIzaSyCdkG6khMtPztesRm1ayY2Qtw3XLj4mEzI";
+// export const FIREBASE_CONFIG = {
+//     apiKey: "AIzaSyDDK5O3i0wU-Ra54BEpxIJWlzhX_XV5kgU",
+//     authDomain: "realist-db192.firebaseapp.com",
+//     projectId: "realist-db192",
+//     storageBucket: "realist-db192.appspot.com",
+//     messagingSenderId: "941883088980",
+//     appId: "1:941883088980:web:1597f631e0f62b2b8d4dc1",
+//     measurementId: "G-5GW7D1WKJZ"
+//   };
+  export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyCdkG6khMtPztesRm1ayY2Qtw3XLj4mEzI",
+    authDomain: "fullstack-assignment-3b467.firebaseapp.com",
+    projectId: "fullstack-assignment-3b467",
+    storageBucket: "fullstack-assignment-3b467.firebasestorage.app",
+    messagingSenderId: "4571258395",
+    appId: "1:4571258395:web:999e96e94b9fdeeddf62be",
+    measurementId: "G-R3NG9RHJXF"
+  };
